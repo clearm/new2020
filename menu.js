e743329a13eb0cd9dddc7d30b9b5b3ec494092fa
DESKTOP-IBOIH3A\edge_mjh <clearm81@naver.com>
2020-01-14 10:19:06 +0900

    test

--- menu.js
+++ menu.js
@@ -1,3 +1,25 @@
 
 
 console.log("log");
+
+var menu_json = {"About Me": "index.html"
+, "현실" : "menu1.html"
+,"꿈": "menu2.html"
+,"목표": "menu3.html"
+,"성과":"menu4.html"
+}
+
+function fn_makeMenu(selected_key)
+{
+    var menu_li = '';
+    $.each(menu_json, function(key,val) {
+        var sel = (key == selected_key) ? "active" : "";
+
+        menu_li += '<li class="nav-item"><a class="nav-link '+sel+'" href="'+val+'"> '+key+'</a></li>';
+    });
+
+    $("#collapsibleNavbar ul > *").remove();
+    $("#collapsibleNavbar ul ").append(menu_li);
+}
+
+fn_makeMenu(selected_menu);
